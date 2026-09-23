8b0f05aad391893fd6d0e23e68e548983198cbc1
azizonpipison <azizonpipison@gmail.com>
2026-09-23 17:08:03 +0500

    for database

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import numpy as np
+from pathlib import Path
+import matplotlib.pyplot as plt
+import soundfile as sf
 
+dataset_path = Path("Noise")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+files = list(dataset_path.glob("*.wav"))
 
+print(f"Найдено файлов: {len(files)}")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+for file in files:
+    audio, sr = sf.read(file)
+    fft = np.fft.rfft(audio)
+    frequencies = np.fft.rfftfreq(len(audio), 1 / sr)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    amplitude = np.abs(fft)
+
+    plt.figure(figsize=(10, 4))
+
+    plt.plot(frequencies, amplitude)
+
+    plt.xlabel("Frequency, Hz")
+    plt.ylabel("Amplitude")
+
+    plt.xlim(0, 5000)
+
+    plt.show()
+
